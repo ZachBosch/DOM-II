3589347cda6f08186992620bd7ed6a13c0afb6fa
Zulu Bravo <zulubravo@zulus-mbp.myfiosgateway.com>
2021-05-04 16:19:47 -0400

    created variables

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+//VARIABLES
+const header = document.querySelector('header');
+const logoHeading = document.querySelector('.logo-heading');
+const nav = document.querySelector('.nav-link:nth-of-type(2)');
+const intro = document.querySelector('.intro');
+const boat = document.querySelector('.img-fluid');
+const footer = document.querySelector('.footer')
+console.log("head", nav);
